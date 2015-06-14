f4123887800374f24ac206f9a63cc37826c7a2fb
JESSICACORD <jessicaleecord@gmail.com>
2015-06-14 15:24:23 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 $(document).foundation();
+
+$(document).ready(function(){
+
+  $('.my-slider').slick({
+    //settings go here
+    autoplay: true,
+    dots: true,
+  });
+
+});
+
+$('.button.next').click(function(){
+  $('.my-slider').slick('slickNext');
+
+});
+
+
+ 
